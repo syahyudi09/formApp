55a7542b1db19aa7d7c1c8d1a21d31fb7c92a7dc
yudi <syahyudi996@gmail.com>
2023-09-27 21:13:10 +0700

    model answer

---
+++ model/AnswerModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const Schema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    formId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    
+    createdAt: {
+        type: Number
+    },
+    updatedAt:  {
+        type: Number
+    }
+    
+},
+{
+    // timestamps akan mengisi createdAt dan updatedAt secara otomatis
+    timestamps: {
+        currentTime: () => Math.floor(Date.now() / 1000),
+    },
+    strict: false
+});
+
+export default mongoose.model('Answer', Schema);
